605676b10f46ce04b771bacde80bb5b5b07f9862
Torsten Raudssus <torsten@raudss.us>
2026-05-18 19:09:54 +0200

    increment $VERSION after 0.103 release

--- lib/Dist/Zilla/Plugin/Docker/API.pm
+++ lib/Dist/Zilla/Plugin/Docker/API.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::Docker::API;
 # ABSTRACT: Build and publish Docker images as Dist::Zilla release artifacts
-our $VERSION = '0.103';
+our $VERSION = '0.104';
 use Moose;
 with 'Dist::Zilla::Role::Plugin';
 with 'Dist::Zilla::Role::AfterBuild';
--- lib/Dist/Zilla/Plugin/Docker/API/TagTemplate.pm
+++ lib/Dist/Zilla/Plugin/Docker/API/TagTemplate.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::Docker::API::TagTemplate;
 # ABSTRACT: Template expansion for Docker image tags
-our $VERSION = '0.103';
+our $VERSION = '0.104';
 use Moo;
 
 has zilla => (
